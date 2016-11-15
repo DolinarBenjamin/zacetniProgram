a5ba60e01532f01cecfe28f9f035d54c9e5f18ea
Nejc Lončar <loncarnej@gmail.com>
2016-11-15 22:18:00 +0100

    hsd

--- Kmetovanje/Kmetovanje/Form1.Designer.cs
+++ Kmetovanje/Kmetovanje/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(473, 68);
+            this.button1.Location = new System.Drawing.Point(461, 109);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -55,7 +55,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(460, 246);
+            this.button3.Location = new System.Drawing.Point(461, 167);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
